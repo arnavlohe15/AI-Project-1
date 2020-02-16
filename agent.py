ba93cdf82b9c92e8b292de0014e283a14b633e5f
Niranjana <niranjana.ganesh@gmail.com>
2020-02-16 16:28:53 -0500

    Updated dfs and bfs

--- agent.py
+++ agent.py
@@ -1,13 +1,14 @@
 
 class Agent:
-    global grid
-    def __init__(self, my_grid):
-        grid = my_grid
+    def __init__(self, grid):
+        # STORE GRID
+        self.grid = grid
+
         dim = len(grid)
 
         # INIT POS = [0,0], GOAL POS = [DIM-1, DIM-1]
-        self.init_pos = [0, 0]
-        self.goal_pos = [dim-1, dim-1]
+        self.init_pos = (0, 0)
+        self.goal_pos = (dim-1, dim-1)
 
         # SETUP FRINGE/CLOSED SET
         self.fringe = [] # unvisited nodes
@@ -15,25 +16,63 @@ class Agent:
         self.closed = [] # visited nodes
 
     def dfs(self):
-        while len(self.fringe) != 0:
-            curr_pos = self.fringe.pop()
+        # fringe is stack
+        while len(self.fringe) != 0: # while fringe is not empty
+            curr_pos = self.fringe.pop() # get current state
             if curr_pos == self.goal_pos:
-                print ('Success!')
-
+                print("Success!")
+                print("Current pos: ", curr_pos)
+                print("Path: ", self.closed)
+                return
+            else:
+                for child in self.get_children(curr_pos):
+                    out_of_bounds = child[0] < 0 or \
+                                    child[0] >= len(self.grid) or \
+                                    child[1] < 0 or \
+                                    child[1] >= len(self.grid)
+                    if not out_of_bounds:
+                        invalid = self.grid[child[0]][child[1]] == 1
+                        if not invalid and child not in self.closed:
+                            self.fringe.append(child)
+                self.closed.append(curr_pos)
+        print("No solution")
+        return
 
     def bfs(self):
-        print('bfs')
+        # fringe is stack
+        while len(self.fringe) != 0:  # while fringe is not empty
+            curr_pos = self.fringe.pop(0)  # get current state
+            if curr_pos == self.goal_pos:
+                print("Success!")
+                print("Current pos: ", curr_pos)
+                print("Path: ", self.closed)
+                return
+            else:
+                for child in self.get_children(curr_pos):
+                    out_of_bounds = child[0] < 0 or \
+                                    child[0] >= len(self.grid) or \
+                                    child[1] < 0 or \
+                                    child[1] >= len(self.grid)
+                    if not out_of_bounds:
+                        invalid = self.grid[child[0]][child[1]] == 1
+                        if not invalid and child not in self.closed:
+                            self.fringe.append(child)
+                self.closed.append(curr_pos)
+        print("No solution")
+        return
 
     def a_star_euclidean(self):
         print('a_star_euclidean')
 
-
     def a_star_manhattan(self):
         print('a_star_manhattan')
-
 
     def bidirectional_bfs(self):
         print('bidirectional_bfs')
 
+    def get_children(self, curr_pos):
+        row = curr_pos[0] # access row of tuple
+        col = curr_pos[1] # access col of tuple
+        return [(row+1, col), (row-1, col), (row, col+1), (row, col-1)]
 
 
--- driver.py
+++ driver.py
@@ -5,64 +5,71 @@ import matplotlib.pyplot as plt
 plt.imshow(np.random.random((10,10)));
 plt.colorbar()
 plt.show()"""
+
 import numpy as np
+from agent import Agent
 import pygame
 
+# SETUP GRID
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BACKGROUND = (79, 159, 159)
+RED = (255, 0, 0)
+MAROON = (176, 48, 96)
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 40
+HEIGHT = 40
+# This sets the margin between each cell
+MARGIN = 5
+
+# Get dimension input
+#dim = int(input('Enter dimension: '))
+dim = 5
+
+# Set the width and height of the screen [width, height]
+size = ((MARGIN + WIDTH) * dim + MARGIN, (MARGIN + WIDTH) * dim + MARGIN)
+screen = pygame.display.set_mode(size)
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
 def main():
-
-    # SETUP GRID
-    # Define some colors
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    GREEN = (0, 255, 0)
-    BACKGROUND = (79, 159, 159)
-    RED = (255, 0, 0)
-
-    # This sets the WIDTH and HEIGHT of each grid location
-    WIDTH = 40
-    HEIGHT = 40
-
-    # This sets the margin between each cell
-    MARGIN = 5
-
     # Create a 2 dimensional array. A two dimensional
     # array is simply a list of lists.
     grid = []
-    # Get dimension input
-    dim = int(input('Enter dimension: '))
-
-    for row in range(dim):
-        # Add an empty array that will hold each cell
-        # in this row
-        grid.append([])
-        for column in range(dim):
-            grid[row].append(np.random.binomial(1, 0.5, 1))  # Append a cell
-
-    print(grid)
-
-    pygame.init()
-
-    # Set the width and height of the screen [width, height]
-    size = ((MARGIN + WIDTH) * dim + MARGIN, (MARGIN + WIDTH) * dim + MARGIN)
-    screen = pygame.display.set_mode(size)
 
     pygame.display.set_caption("Maze Runner")
 
     # Loop until the user clicks the close button.
     done = False
 
-    # Used to manage how fast the screen updates
-    clock = pygame.time.Clock()
+    for row in range(dim):
+        # Add an empty array that will hold each cell
+        # in this row
+        grid.append([])
+        for column in range(dim):
+            grid[row].append(np.random.binomial(1, 0.2, 1))  # Append a cell
+        grid[0][0] = 0
+
+    #pygame.init()
+
+    # Initialize agent
+    agent = Agent(grid)
+    print("DFS")
+    agent.dfs()
+    print("BFS")
+    agent.bfs()
 
     # -------- Main Program Loop -----------
-    while not done:
+    """while not done:
         # --- Main event loop
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
                 done = True
 
         # --- Game logic should go here
-
 
         # --- Screen-clearing code goes here
 
@@ -75,26 +82,67 @@ def main():
 
         # --- Drawing code should go here
         # Draw the grid
-        for row in range(10):
-            for column in range(10):
-                color = WHITE
-                if grid[row][column] == 1:
-                    color = GREEN
+        for row in range(dim):
+            for column in range(dim):
+                if grid[row][column] == 0:
+                    color = WHITE
+                else:
+                    color = BLACK
+                #if row == 0 and column == 0:
+                    #color = WHITE
+                # color = WHITE
                 pygame.draw.rect(screen,
                                  color,
                                  [(MARGIN + WIDTH) * column + MARGIN,
                                   (MARGIN + HEIGHT) * row + MARGIN,
                                   WIDTH,
                                   HEIGHT])
-
         # --- Go ahead and update the screen with what we've drawn.
         pygame.display.flip()
 
         # --- Limit to 60 frames per second
         clock.tick(60)
 
+        for row in range(dim):
+            for column in range(dim):
+                for pos in agent.closed:
+                    if row == pos[0] and column == pos[1]:
+                        color = MAROON
+                    pygame.draw.rect(screen,
+                                     color,
+                                     [(MARGIN + WIDTH) * column + MARGIN,
+                                      (MARGIN + HEIGHT) * row + MARGIN,
+                                      WIDTH,
+                                      HEIGHT])
+                    pygame.display.update()
+
     # Close the window and quit.
-    pygame.quit()
+    quit()"""
+
+def show_dfs(agent, grid):
+    # look at agent's closed states
+        # color the squares in closed states maroon
+    print('entered show_dfs')
+
+    for pos in agent.closed:
+        if row is pos()[0] and col is pos()[1]:
+            color = MAROON
+        pygame.draw.rect(screen,
+                        color,
+                        [(MARGIN + WIDTH) * column + MARGIN,
+                        (MARGIN + HEIGHT) * row + MARGIN,
+                        WIDTH,
+                        HEIGHT])
+    pygame.display.flip()
+
+def show_bfs(agent, grid):
+    return
+def show_a_star_euclidean(agent, grid):
+    return
+def show_a_star_manhattan(agent, grid):
+    return
+def show_bidirectional_bfs(agent, grid):
+    return
 
 
 if __name__ == '__main__':
