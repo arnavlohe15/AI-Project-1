848ff75394472b0b25e381824c45bc0c7ed44e6a
Arnav Lohe <arnavlohe15@gmail.com>
2020-02-16 22:44:31 -0500

    updated driver

--- driver.py
+++ driver.py
@@ -1,33 +1,27 @@
 #creating a 2d grid in Python
-"""import numpy as np
-import matplotlib.pyplot as plt
-
-plt.imshow(np.random.random((10,10)));
-plt.colorbar()
-plt.show()"""
 
 import numpy as np
 from agent import Agent
 import pygame
 
 # SETUP GRID
-# Define some colors
+# Defining some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 BACKGROUND = (79, 159, 159)
 RED = (255, 0, 0)
-MAROON = (176, 48, 96)
+MAROON = (142,0,144)
 # This sets the WIDTH and HEIGHT of each grid location
 WIDTH = 40
 HEIGHT = 40
 # This sets the margin between each cell
 MARGIN = 5
 
-# Get dimension input
+# Getting dimension input
 dim = int(input('Enter dimension: '))
 
-# Set the width and height of the screen [width, height]
+# Setting the width and height of the screen [width, height]
 size = ((MARGIN + WIDTH) * dim + MARGIN, (MARGIN + WIDTH) * dim + MARGIN)
 screen = pygame.display.set_mode(size)
 
@@ -41,7 +35,7 @@ def main():
 
     pygame.display.set_caption("Maze Runner")
 
-    # Loop until the user clicks the close button.
+    # Looping until the user clicks the close button.
     done = False
 
     for row in range(dim):
@@ -54,96 +48,158 @@ def main():
 
     pygame.init()
 
-    # Initialize agent
+    # Initializing agent and obtaining paths
     agent = Agent(grid)
-
-    print("----------------------")
-    print("DFS")
-    print(agent.dfs())
-    print("----------------------")
-    print("BFS")
-    print(agent.bfs())
-    print("----------------------")
-    print("A Star Euclidean")
-    print(agent.a_star_euclidean())
-    print("----------------------")
-    print("A Star Manhattan")
-    print(agent.a_star_manhattan())
-    print("----------------------")
+    dfs_path = agent.dfs()
+    bfs_path = agent.bfs()
+    #euclid_path = agent.a_star_euclidean())
+    #manhattan_path = agent.a_star_manhattan())
 
 
-
-
-
-    # -------- Main Program Loop -----------
+    # -------- Obtaining Blank Maze -----------
     while not done:
-        # --- Main event loop
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 done = True
-
-        # --- Game logic should go here
-
-        # --- Screen-clearing code goes here
-
-        # Here, we clear the screen to white. Don't put other drawing commands
-        # above this, or they will be erased with this command.
-
-        # If you want a background image, replace this clear with blit'ing the
-        # background image.
         screen.fill(BACKGROUND)
-
-        # --- Drawing code should go here
-        # Draw the grid
         for row in range(dim):
             for column in range(dim):
                 if grid[row][column] == 0:
                     color = WHITE
                 else:
                     color = BLACK
-                #if row == 0 and column == 0:
-                    #color = WHITE
-                # color = WHITE
                 pygame.draw.rect(screen,
                                  color,
                                  [(MARGIN + WIDTH) * column + MARGIN,
                                   (MARGIN + HEIGHT) * row + MARGIN,
                                   WIDTH,
                                   HEIGHT])
-        # --- Go ahead and update the screen with what we've drawn.
         pygame.display.flip()
-
-        # --- Limit to 60 frames per second
         clock.tick(60)
 
-    # Close the window and quit.
+    # -------- Displaying DFS Path -----------
+    done = False
+    while not done:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                done = True
+        screen.fill(BACKGROUND)
+        for row in range(dim):
+            for column in range(dim):
+                if grid[row][column] == 0:
+                    if (row, column) in dfs_path:
+                        color = MAROON
+                    else:
+                        color = WHITE
+                else:
+                    color = BLACK
+                pygame.draw.rect(screen,
+                                 color,
+                                 [(MARGIN + WIDTH) * column + MARGIN,
+                                  (MARGIN + HEIGHT) * row + MARGIN,
+                                  WIDTH,
+                                  HEIGHT])
+        pygame.display.flip()
+        clock.tick(60)
+
+    # -------- Displaying BFS Path -----------
+    done = False
+    while not done:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                done = True
+        screen.fill(BACKGROUND)
+        for row in range(dim):
+            for column in range(dim):
+                if grid[row][column] == 0:
+                    if (row, column) in bfs_path:
+                        color = MAROON
+                    else:
+                        color = WHITE
+                else:
+                    color = BLACK
+                pygame.draw.rect(screen,
+                                 color,
+                                 [(MARGIN + WIDTH) * column + MARGIN,
+                                  (MARGIN + HEIGHT) * row + MARGIN,
+                                  WIDTH,
+                                  HEIGHT])
+        pygame.display.flip()
+        clock.tick(60)
     quit()
 
-def show_dfs(agent, grid):
-    # look at agent's closed states
-        # color the squares in closed states maroon
-    print('entered show_dfs')
+    """# -------- Displaying A* Euclidean Path -----------
+    while not done:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                done = True
+        screen.fill(BACKGROUND)
+        for row in range(dim):
+            for column in range(dim):
+                if grid[row][column] == 0:
+                    if (row, column) in euclid_path:
+                        color = MAROON
+                    else:
+                        color = WHITE
+                else:
+                    color = BLACK
+                pygame.draw.rect(screen,
+                                 color,
+                                 [(MARGIN + WIDTH) * column + MARGIN,
+                                  (MARGIN + HEIGHT) * row + MARGIN,
+                                  WIDTH,
+                                  HEIGHT])
+        pygame.display.flip()
+        clock.tick(60)
+    quit()
 
-    for pos in agent.closed:
-        if row is pos()[0] and col is pos()[1]:
-            color = MAROON
-        pygame.draw.rect(screen,
-                        color,
-                        [(MARGIN + WIDTH) * column + MARGIN,
-                        (MARGIN + HEIGHT) * row + MARGIN,
-                        WIDTH,
-                        HEIGHT])
-    pygame.display.flip()
+    # -------- Displaying A* Manhattan Path -----------
+    while not done:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                done = True
+        screen.fill(BACKGROUND)
+        for row in range(dim):
+            for column in range(dim):
+                if grid[row][column] == 0:
+                    if (row, column) in manhattan_path:
+                        color = MAROON
+                    else:
+                        color = WHITE
+                else:
+                    color = BLACK
+                pygame.draw.rect(screen,
+                                 color,
+                                 [(MARGIN + WIDTH) * column + MARGIN,
+                                  (MARGIN + HEIGHT) * row + MARGIN,
+                                  WIDTH,
+                                  HEIGHT])
+        pygame.display.flip()
+        clock.tick(60)
+    quit()
 
-def show_bfs(agent, grid):
-    return
-def show_a_star_euclidean(agent, grid):
-    return
-def show_a_star_manhattan(agent, grid):
-    return
-def show_bidirectional_bfs(agent, grid):
-    return
+    # -------- Displaying Bidirectional BFS Path -----------
+    while not done:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                done = True
+        screen.fill(BACKGROUND)
+        for row in range(dim):
+            for column in range(dim):
+                if grid[row][column] == 0:
+                    color = WHITE
+                else:
+                    color = BLACK
+                pygame.draw.rect(screen,
+                                 color,
+                                 [(MARGIN + WIDTH) * column + MARGIN,
+                                  (MARGIN + HEIGHT) * row + MARGIN,
+                                  WIDTH,
+                                  HEIGHT])
+        pygame.display.flip()
+        clock.tick(60)
+    quit()"""
 
-
+#--------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     main()
